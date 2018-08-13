09881d97f9fa54026bdb2d6554b2d1361c7617cf
Zohayb Shaikh <zohayb1996@gmail.com>
2018-08-13 11:22:01 -0400

    seed

--- database/models/art.js
+++ database/models/art.js
@@ -1,21 +1,21 @@
-const Sequelize = require("sequelize");
-const db = require("../db");
+const Sequelize = require('sequelize');
+const db = require('../database');
 
-const Art = db.define("art", {
+const Art = db.define('art', {
   artPiece: {
     type: Sequelize.JSONB,
-    allowNull: false
+    allowNull: false,
   },
   location: {
     type: Sequelize.ARRAY(Sequelize.FLOAT),
-    allowNull: false 
+    allowNull: false,
   },
   description: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   likes: {
-    type: Sequelize.INTEGER
-  }
-})
+    type: Sequelize.INTEGER,
+  },
+});
 
-module.exports = Art
+module.exports = Art;
